2986d7c1b2c132a4765796d030ff006268ed2426
nickzuck <nikhil1945.singh@gmail.com>
2015-10-08 01:17:36 +0530

    AC...galat question padha tha

--- 266A_stones_on_the_table.py
+++ 266A_stones_on_the_table.py
@@ -1,15 +1,3 @@
 n = int(raw_input())
-countMin = n 
 s = raw_input()
-count = 0  
-prev = s[0]
-for i in range(1 , len(s)):
-	if (s[i] == prev):
-		count += 1
-	else :
-		if (countMin > count):
-			countMin = count 
-if countMin == n :
-	countMin -= 1 
-
-print countMin
+print (sum( 1 for i in range(0,n-1) if s[i] == s[i+1]))
