550c5955adedff57e8d72dfde87d12c4c3b9bac8
nickzuck <nikhil1945.singh@gmail.com>
2015-10-08 01:42:58 +0530

    Finally AC .....Again :( ..read the question in a wrong way

--- 131A_caps_lock.py
+++ 131A_caps_lock.py
@@ -1,21 +1,8 @@
 a = raw_input()
-if(a.upper()== a):
-	b = a.lower() 
-	c = a[0].upper()
-	d = c+b[1:]
-	d = d.strip()
-	print d 
-elif(a[0].islower()):
-	b = a[1:]
-	if (b.upper() == b):
-		c = a.lower()
-		d = a[0].upper()
-		d = d+c[1:]
-		d = d.strip()
-		print d
-	else :
-		print a
+#the capslock was turned on accidently
+if (a.upper() == a or a[0].upper()+a[1:] == a.upper()):
+	print ''.join(i.lower() if i.isupper() else i.upper() for i in a)
 else:
-	print "yes kahan pahunch gaya"
-	print a		
-	
+	print a 
+
+   
