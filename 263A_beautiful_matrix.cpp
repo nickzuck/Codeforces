4102b9c53daeda54a2e88f38e910b59c30a97548
nickzuck <nikhil1945.singh@gmail.com>
2015-10-08 08:51:29 +0530

    AC in one go :)

---
+++ 263A_beautiful_matrix.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std ; 
+
+int main ()
+{
+	int arr[10][10] ; 
+	int i , j , a , b; 
+	for(i = 1 ; i<=5 ; i++){
+		for (j =1 ; j<=5 ;j++){
+			cin >> arr[i][j] ;
+			if (arr[i][j] == 1){
+				a = i ; 
+				b = j ;
+			}	
+		}
+	}
+	
+	int ans = abs(3-a) + abs(3-b) ; 
+	cout << ans << endl ; 
+return 0 ; 
+}
