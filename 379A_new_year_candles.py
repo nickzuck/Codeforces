000681ca9e5134b1b43c4c756592d6a5e5741daf
nickzuck <nikhil1945.singh@gmail.com>
2015-10-13 12:34:14 +0530

    WAs messing up the things ...REASON : Bad variable name..Help from internet :(

--- 379A_new_year_candles.py
+++ 379A_new_year_candles.py
@@ -1,17 +1,8 @@
 a , b = map(int , raw_input().split())
 count = a 
-#c indicated new set of candles to be burnt
-c = a/b
-#rem indicates the burnt candles which can't be transformed
-rem = a%b 
-count += c
-while (True):
-	c = (c + rem) / b
-	rem = (c+rem)%b
-	if (c + rem < b):
-		break
-	count += c
-print count
-	
-
-	
+while (a >=b):
+	div = a/b 
+	rem = a%b 
+	count += div 
+	a = div+rem 
+print count 
