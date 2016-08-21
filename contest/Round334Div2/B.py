84d9c6f53eeac950b33d26dcb449768f9d349247
nickzuck <nikhil1945.singh@gmail.com>
2016-08-21 16:24:14 +0530

    Finally AC

--- contest/Round334Div2/B.py
+++ contest/Round334Div2/B.py
@@ -1,17 +1,12 @@
 n, k = map(int, raw_input().split())
 s = map(int, raw_input().split())
+orgK = k
 k = k - (n/2)
 if k > 0:
     kAns = max(s[n-k:])
     ans = kAns
 else:
     ans = 0
-j = n-k-1
-i = 0 
-while(i < j):
-    temp = s[i] + s[j]
-    if temp > ans :
-        ans = temp 
-    i += 1 
-    j -= 1
+for i in range(0, n-orgK):
+    ans = max(ans, s[i] + s[2*(n-orgK) -1 - i])
 print ans
