312eccfa51ba13e92d8b5532b0bea847e6e7bf62
nickzuck <nikhil1945.singh@gmail.com>
2017-06-04 13:38:31 +0530

    Getting WA

---
+++ educational/Round21/C.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+inline int  ceil(int a){
+    if(a%2==0){
+        return a/2;
+    } 
+    else
+        return (a/2+1);
+}
+
+int main()
+{
+    int n, w, temp;
+    cin >> n >> w ; 
+    int a[n+1], p[n+1] ;
+    memset(p, 0, sizeof(p));
+    int sh = 0 ;
+    for(int i = 0 ; i< n ;i++){
+        cin >> temp;
+        sh += ceil(temp) ;
+        a[i] = temp ; 
+    }
+    bool flag = true ;
+    if(sh > w){
+        flag = false ;
+    }
+    else{
+        int rem = w - sh, max, mi ;
+
+        for(int i = 0 ;i < n ; i++){
+            if(rem <= 0)
+                break ;
+            max = a[0];
+            mi = 0 ;
+            for(int j = 1 ; j < n ; j++){
+                if(a[j] > max and p[j] == 0){
+                    max = a[j] ;  mi = j ;
+                }
+            }
+            temp = floor(a[mi]/2);
+            p[mi] = min(rem, temp)+ ceil(a[mi]);
+            rem -=  min(rem, temp);
+        }
+    }
+    if(flag){
+        for(int i = 0 ;i < n ; i++){
+            if(p[i] == 0){
+                cout << ceil(a[i]) << " ";
+            }
+            else
+                cout << p[i] << " " ;
+        }
+        cout << endl ;
+    }
+    else{
+        cout << -1 << endl ;
+    }
+return 0 ;
+}
