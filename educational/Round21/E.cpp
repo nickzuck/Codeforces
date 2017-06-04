421c2208a9498f8e2ba3237dd1105bd814a40720
nickzuck <nikhil1945.singh@gmail.com>
2017-06-04 14:18:26 +0530

    Wrong Anwer :|

--- educational/Round21/E.cpp
+++ educational/Round21/E.cpp
@@ -1,68 +1,34 @@
 #include<bits/stdc++.h>
 
-#define MAX 503
-using namespace std; 
+#define mp make_pair
+#define pb push_back
 
-int knap[2][2000002] = {0};
-void print(int val[], int weight[], int n , int maxW){
-    for(int i = 0 ; i <= maxW; i++){
-        cout << knap[0][i] << " " ;
-    }
-    cout << endl ;
-    for(int i = 0 ; i <= maxW; i++){
-        cout << knap[1][i] << " " ;
-    }
-    cout << endl ;
-}
-int knapsack(int val[], int weight[], int n , int maxW){
-    bool a  ;
+using namespace std ;
 
-    //Testing 
-    // for(int i = 0 ; i <= maxW; i++){
-    //     cout << knap[0][i] << " " ;
-    // }
-    // cout << endl ;
-    // for(int i = 0 ; i <= maxW; i++){
-    //     cout << knap[1][i] << " " ;
-    // }
-    // cout << endl ;
-    //Testing ends here
-
-    for(int i = 1 ; i <=n ;i ++){
-        a = i%2 ;
-        for(int j = 1 ; j <= maxW ; j ++){
-            if(weight[i-1] <= j){
-                knap[a][j] = max( knap[!a][j- weight[i-1]] + val[i-1], 
-                                  knap[!a][j]
-                                ) ;
-            }
-            else{
-                knap[a][j] = knap[!a][j];
-            }
-        }
-        //print(val, weight, n, maxW );
-    }
-    //cout << knap[1][maxW] << " " << knap[0][maxW]  << endl;
-    if(n&1){
-        return knap[1][maxW];
-    }
-    else
-        return knap[0][maxW];
-}
-
-int main(){
-    int n, maxW ; 
-    int val[MAX], weight[MAX] ;
-    
-    //Input
-    cin >> n >> maxW;
+int main()
+{
+    int maxW,  n;
+    int value , w ;
+    vector<pair<int, int> > v ;
+    cin >> n >> maxW ;
     for(int i = 0 ;i < n ; i++){
-        cin >> weight[i] >> val[i];
+        cin >> w >> value ;
+        v.pb(mp(value, w));
+    }
+    sort(v.begin(), v.end()) ;
+    reverse(v.begin(), v.end());
+    
+    long long  currV = 0 ,currW = 0 ; 
+    for(int i = 0 ;i < n ; i++){
+        if(currW >= maxW )
+            break ;
+        if(currW + v[i].second <= maxW){
+            currV += v[i].first ;   
+            currW += v[i].second ; 
+        }
     }
 
-    //Processing
-    int ans ; 
-    ans = knapsack(val, weight, n , maxW);
-    cout << ans << endl;
-    return 0 ;
+    cout << currV << endl ;
+    
+return 0 ;
 }
